39e160d5bdc3c187451c6c97dc02b37dd410f58d
liyang <admin@open-laboratory.org>
2025-07-01 20:26:05 +0000

    a

--- vrc10/tests/btg-stake-mint.ts
+++ vrc10/tests/btg-stake-mint.ts
@@ -6,6 +6,8 @@ import { TOKEN_2022_PROGRAM_ID } from "open-token-web3";
 describe("btg-stake-mint", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
+  const keypair  = anchor.web3.Keypair.fromSecretKey(Buffer.from([16,4,81,162,237,130,82,129,66,194,184,173,91,81,185,175,236,116,28,178,12,103,216,254,6,8,42,78,115,167,148,245,116,19,214,53,118,216,188,3,212,136,185,128,226,26,6,53,206,34,164,82,169,134,129,109,74,78,106,250,248,28,18,137]));
+
   anchor.setProvider(provider);
   const program = anchor.workspace.BtgStakeMint as Program<BtgStakeMint>;
   const [configPda, bump] =  anchor.web3.PublicKey.findProgramAddressSync(
@@ -15,47 +17,47 @@ describe("btg-stake-mint", () => {
     console.log("Config PDA:", configPda.toBase58());
   const mint = new anchor.web3.PublicKey("E4vDrY61iWbsWso11fvPxNYFyZb7sMUSkrbCKRLbV4Wj")
 
-  // it("Is initialized!", async function(){
-  //   this.timeout(100000);
-  //   // Add your test here.
-  //   const tx = await program.methods.initialize().accounts([
-  //     { pubkey: provider.wallet.publicKey, isWritable: true, isSigner: true },
-  //   ]).rpc();
-  //   console.log("Your transaction signature", tx);
-  // });
+  it("Is initialized!", async function(){
+    this.timeout(100000);
+    // Add your test here.
+    const tx = await program.methods.initialize().accounts([
+      { pubkey: keypair.publicKey, isWritable: true, isSigner: true },
+    ]).rpc();
+    console.log("Your transaction signature", tx);
+  });
 
-  // it("Add to whitelist", async function() {
-  //   this.timeout(100000);
-  //   const tx = await program.methods.addToWhitelist("BTG", 0.5)
-  //   .accounts({ authority:provider.wallet.publicKey, mint:mint}).rpc();
-  //   console.log("Add to whitelist tx:", tx);
+  it("Add to whitelist", async function() {
+    this.timeout(100000);
+    const tx = await program.methods.addToWhitelist("BTG", 0.5)
+    .accounts({ authority:keypair.publicKey, mint:mint}).rpc();
+    console.log("Add to whitelist tx:", tx);
    
-  // });
+  });
 
-  // it("Remove from whitelist", async function() {
-  //   this.timeout(100000);
-  //   const tx = await program.methods.removeFromWhitelist()
-  //   .accounts({ authority:provider.wallet.publicKey, mint:mint}).rpc();
-  //   console.log("Remove from whitelist tx:", tx);
-  // });
-  // it("Stake", async function() { 
-  //   this.timeout(100000);
-  //   const keypair = anchor.web3.Keypair.generate()
-  //   const tx = await program.methods.stakeBtg(new anchor.BN(1000000000))
-  //   .accounts({ stakingVault: keypair.publicKey,
-  //     user:provider.wallet.publicKey, 
-  //     mint:mint,
-  //     tokenProgram: TOKEN_2022_PROGRAM_ID,
+  it("Remove from whitelist", async function() {
+    this.timeout(100000);
+    const tx = await program.methods.removeFromWhitelist()
+    .accounts({ authority:keypair.publicKey, mint:mint}).rpc();
+    console.log("Remove from whitelist tx:", tx);
+  });
+  it("Stake", async function() { 
+    this.timeout(100000);
+    const keypair = anchor.web3.Keypair.generate()
+    const tx = await program.methods.stakeBtg(new anchor.BN(1000000000))
+    .accounts({ stakingVault: keypair.publicKey,
+      user:keypair.publicKey, 
+      mint:mint,
+      tokenProgram: TOKEN_2022_PROGRAM_ID,
     
-  //   }).signers([keypair]).rpc();
-  // })
+    }).signers([keypair]).rpc();
+  })
 
   it("RedeemToken", async function() { 
     this.timeout(100000);
     const tx = await program.methods.redeemToken()
     .accounts({
       stakingVault: new anchor.web3.PublicKey("EE9RHTK9LwjLSGbkwUojpancEhnN8jdPXH2czdywbJBy"),
-      user:provider.wallet.publicKey, 
+      user:keypair.publicKey, 
       mint:mint,
       tokenProgram: TOKEN_2022_PROGRAM_ID,
     }).rpc();
@@ -68,7 +70,7 @@ describe("btg-stake-mint", () => {
       {
         memcmp: {
           offset: 8, // user 字段偏移量
-          bytes: provider.wallet.publicKey.toBase58(), // 匹配的公钥字符串
+          bytes: keypair.publicKey.toBase58(), // 匹配的公钥字符串
         },
       },
     ];
